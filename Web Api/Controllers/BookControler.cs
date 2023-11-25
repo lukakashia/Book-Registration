e3fa23b13f2e0028eb449010c6b6760aa1a010c7
DESKTOP-9FT8NSD\THERE WAS PIPIKO <lukakashia10@gmail.com>
2023-11-24 20:38:36 -0800

    Initial

---
+++ Web Api/Controllers/BookControler.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Api;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class BookControler : ControllerBase
+    {
+        private static List<Book> _books = new List<Book>();
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            return Ok(_books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(Book book)
+        {
+            book.Id = _books.Count + 1;
+            _books.Add(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            book.Name = updatedBook.Name;
+            book.Author = updatedBook.Author;
+            return NoContent();
+        }
+
+        // DELETE: api/books/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _books.Remove(book);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Web Api/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Web_Api;
+using WebApplication2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.BuildServiceProvider();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<Book>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+}
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
